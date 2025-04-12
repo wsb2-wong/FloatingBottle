e05b60c7afdd811b3320e45385b265468827b243
Beatrice Wong <wingsum0855@gmail.com>
2025-04-12 19:30:34 +0100

    Interaction1
    trying tap interaction.

--- Assets/Scripts/ImageTracker.cs
+++ Assets/Scripts/ImageTracker.cs
@@ -17,26 +17,19 @@ public class ImageTracker : MonoBehaviour
 
     void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-        AudioSource source = GetComponent<AudioSource>();
 
         foreach (ARTrackedImage newImage in eventArgs.added)
         {
             Debug.Log("Found image");
 
-            // Instantiate the bottle prefab
-            GameObject newObject = Instantiate(Bottle);
 
-            // Parent it to the marker so it follows the image
+            GameObject newObject = Instantiate(Bottle);
             newObject.transform.SetParent(newImage.transform, false);
 
             // Position offset - move a bit forward relative to the marker (e.g., Z = 0.3m)
             //newObject.transform.localPosition = new Vector3(0.1f, 0f, 2f);
             // Scale it so it's 0.17 meters long (assuming uniform scale, adjust as needed)
             //newObject.transform.localScale = Vector3.one * 0.17f;
-
-            //source.Play();
-
-            // OPTIONAL: Example of how you can also randomly spawn rock instances
 
         }
     }
